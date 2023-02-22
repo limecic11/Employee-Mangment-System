9397853f946e2cfa7c9b9574da02d3bda0af9ede
Spider <121188879+limecic11@users.noreply.github.com>
2023-02-22 22:19:40 +0200

    Add files via upload

---
+++ Emp mang/src/emp/mang/EmpMang.java
@@ -0,0 +1,10 @@
+
+package emp.mang;
+
+public class EmpMang {
+    public static void main(String[] args) {
+       Log_in l= new Log_in();
+       l.setVisible(true);
+       l.setResizable(false);
+    }   
+}
